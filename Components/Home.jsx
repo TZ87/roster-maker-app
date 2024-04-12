46c46958f2e77f241fc81af423be7bf785145910
Tóth Zoltán <toth.zoltan8719@gmail.com>
2024-04-12 03:08:38 +0200

    feat: reading holidays from freeDays.json

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -10,7 +10,8 @@ function Home() {
   const [selectedMonth, setSelectedMonth] = useState("01");
   const [daysInMonth, setDaysInMonth] = useState(31);
   const [daysArray, setDaysArray] = useState([...Array(daysInMonth).keys()].map(day => day + 1));
-  const [freeDays, setFreeDays] = useState({days:[]});
+  const [freeDays, setFreeDays] = useState([]);
+  const [holidays, setHolidays] =useState([]);
 
   const [monthOptions,setMonthOptions] = useState([
     { value: "01", label: "Január" },
@@ -33,42 +34,33 @@ function Home() {
     setDaysArray([...Array(days).keys()].map(day => day + 1));
   }, [selectedMonth]);
   //console.log(selectedMonth, daysInMonth);
+
+const getData = () => {
+  fetch('freeDays.json', {headers: {
+    'Content-Type' : 'application/json',
+    'Accept': 'application./json'
+  }}).then((response)=>{
+    return response.json()
+  }).then((data)=>{
+    setFreeDays(data);
+  })
+}
+
+useEffect(()=>{
+  getData()
+},[])
  
   const getDaysInMonth = (year, month) => {
      return new Date(year, month, 0).getDate();
   };
+
   const isWeekend = (day) => {
     const date = new Date(selectedYear, selectedMonth - 1, day);
     const dayOfWeek = date.getDay(); 
     return dayOfWeek === 0 || dayOfWeek === 6; 
   };
 
-  useEffect(()=> {
-    const fetchData = async () => {
-      const resp = await fetch(`https://szunetnapok.hu/api/91ba48bb924e7cf64df1db2dcf9648d28fed707ea475eeec6ad1cef91594f2a8/2024`);
-      const data = await resp.json();
-      setFreeDays({days: data});
-      downloadJSON();
-    }
-    fetchData();
-  },[selectedMonth]);
-
-  console.log(freeDays);
-  console.log(freeDays.days.days)
-
-  const downloadJSON = () => {
-    const filename = "freeDays.json";
-    const json = JSON.stringify(freeDays);
-    const blob = new Blob([json], { type: "application/json" });
-    const href = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = href;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
+  
   return (
     <div>
       <h1>Üdvözlöm a Roster Maker applikációban!</h1>
@@ -79,8 +71,7 @@ function Home() {
           <option key={option.value} value={option.value}>{option.label}</option>
         ))}
       </select>
-      <button onClick={downloadJSON}>JSON letöltése</button>
-
+      
       <table>
         <thead>
           <tr>
