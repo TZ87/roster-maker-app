daf14afe78f56f6e25c2dbbec27cc482f744e2da
Tóth Zoltán <toth.zoltan8719@gmail.com>
2024-06-06 19:58:30 +0200

    feat: add employee model

---
+++ Backend/RosterMaker/Model/Employee.cs
@@ -0,0 +1,11 @@
+namespace RosterMaker.Model;
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Position { get; init; }
+    public bool IsPossibleNightShift { get; init; }
+    public string Group { get; init; }
+}
+    
